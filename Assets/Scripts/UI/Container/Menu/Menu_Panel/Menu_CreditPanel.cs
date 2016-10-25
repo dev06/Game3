a39a832e69a7ffe7f00395327cef0103dffa5a2b
dev06 <devanpatel13@gmail.com>
2016-10-25 12:33:03 -0400

    stuff

---
+++ Assets/Scripts/UI/Container/Menu/Menu_Panel/Menu_CreditPanel.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu_CreditPanel : MonoBehaviour {
+
+	private Animation _animation;
+
+	void OnEnable()
+	{
+		EventManager.OnCreditUnactive += OnCreditUnactive;
+		EventManager.OnCreditActive += OnCreditActive;
+	}
+
+	void OnDisable()
+	{
+		EventManager.OnCreditUnactive -= OnCreditUnactive;
+		EventManager.OnCreditActive -= OnCreditActive;
+	}
+
+
+	void OnCreditActive()
+	{
+		PlayAnimation(1);
+		Debug.Log("ACTIVE");
+	}
+
+	void OnCreditUnactive()
+	{
+		PlayAnimation(-1);
+		Debug.Log("UNACTIVE");
+	}
+
+	void Start () {
+		_animation = GetComponent<Animation>();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	public void PlayAnimation(int direction)
+	{
+		if (direction > 0)
+		{
+			_animation[_animation.clip.name].time = 0;
+		} else {
+			_animation[_animation.clip.name].time = _animation[_animation.clip.name].length;
+		}
+		_animation[_animation.clip.name].speed = direction;
+		_animation.Play(_animation.clip.name, PlayMode.StopAll);
+	}
+}
