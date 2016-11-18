5c6cb6cb8272e3712852ede6456b7a4206e04e02
dev06 <devanpatel13@gmail.com>
2016-11-17 19:03:15 -0500

    localChange.

---
+++ Assets/Scripts/UI/Button/Login/Register/LoginMenuButton.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class LoginMenuButton : ButtonEventHandler {
+
+	// Use this for initialization
+	void Start () {
+		Init();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	/// <summary>
+	/// Overrides the on pointer enter from base class.
+	/// </summary>
+	/// <param name="data"></param>
+	public override void OnPointerEnter(PointerEventData data)
+	{
+
+		base.OnPointerEnter(data);
+
+	}
+	/// <summary>
+	/// Overrides the on pointer exit from base class
+	/// </summary>
+	/// <param name="data"></param>
+	public override void OnPointerExit(PointerEventData data)
+	{
+
+		base.OnPointerExit(data);
+
+		//WORKING ON INTERACTIBLE FOR LOAD BUTTON
+
+	}
+
+	/// <summary>
+	/// Overrides the on pointer click from base class
+	/// </summary>
+	/// <param name="data"></param>
+	public override void OnPointerClick(PointerEventData data)
+	{
+		base.OnPointerClick(data);
+
+		if (buttonID == ButtonID.LOGIN)
+		{
+			if (EventManager.OnLoginButtonPress != null)
+			{
+				EventManager.OnLoginButtonPress();
+			}
+		}
+
+		if (buttonID == ButtonID.REGISTER)
+		{
+			if (EventManager.OnRegisterButtonPress != null)
+			{
+				EventManager.OnRegisterButtonPress();
+			}
+		}
+
+
+		if (buttonID == ButtonID.CREATE_MY_PROFILE)
+		{
+			if (EventManager.OnCreateProfileButtonPress != null)
+			{
+				EventManager.OnCreateProfileButtonPress();
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/UI/Indicator/HealthIndicator/HealthIndiciator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class HealthIndiciator : MonoBehaviour {
+
+
+	private Image _fillImage;
+	private Text _text;
+	private Mob _target;
+
+	void Start ()
+	{
+		_fillImage = transform.FindChild("FillImage").GetComponent<Image>();
+		_text = transform.FindChild("Text").GetComponent<Text>();
+		if (transform.parent.GetComponent<Mob>() != null)
+		{
+			_target = transform.parent.GetComponent<Mob>();
+		} else
+		{
+			Debug.Log("Can't attach health indicator to non-mob object");
+		}
+	}
+
+	void Update ()
+	{
+		UpdateComponents();
+	}
+
+	private void UpdateComponents()
+	{
+		_fillImage.fillAmount = _target.GetHealth / _target.GetMaxHealth;
+		_text.text = "" + (int)(_fillImage.fillAmount * _target.GetMaxHealth);
+		transform.LookAt(Camera.main.transform.position);
+	}
+}
